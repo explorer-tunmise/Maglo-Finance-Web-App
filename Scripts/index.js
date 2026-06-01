032c193d4d8c1367b347788ea98ed5060fbb8f10
Tunmise Adewole <adewoletunmise2000@gmail.com>
2026-06-01 20:40:02 +0100

    Enhance login functionality and form validation in index.html and index.js; clean up unused CSS classes in output.css

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,62 +1,55 @@
-"use strict"
-
-// Create a user object
-
+// Create an array of user objects
 let users = [
   {
     name: "adewole tunmise",
     username: "lordEben",
     email: "adewoletunmise2000@gmail.com",
-    password: "152102020"
+    password: "152102020",
   },
 
   {
     name: "john doe",
     username: "johnDoe",
     email: "johndoe@gmail.com",
-    password: "12345678"
+    password: "12345678",
   },
 
   {
     name: "jane smith",
     username: "janeSmith",
     email: "janesmith@yahoo.com",
-    password: "password123"
-  }
+    password: "password123",
+  },
 ];
 
-
-
 // Get the form element
-const form = document.getElementById("login-Form");
+const form = document.getElementById("login-form");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const signIn = document.getElementById("signin-button");
-const overlay = document.getElementById("overlay");
-const errorMessage = document.getElementById("error-message");
 
-
-signIn.addEventListener("click", function (event) {
-    event.preventDefault();
-
-    let emailValue = email.value
-    let passwordValue = password.value
-
-
-    if(!emailValue && !passwordValue){
-        email.classList.add("outline-1", "outline-red-500");
-        password.classList.add("outline-1", "outline-red-500");
-        overlay.classList.remove("hidden")
-        errorMessage.classList.remove("hidden")
-        console.log("Email and password are required");
-    } else if (!passwordValue) {
-        password.classList.add("outline-1", "outline-red-500");
-        console.log("Password is required");
-    } else if (!emailValue) {
-        email.classList.add("outline-1", "outline-red-500");
-        console.log("Email is required");
+function login() {
+  users.find((user) => {
+    if (
+      email.value === "" ||
+      !email.value.includes("@") ||
+      !email.value.includes(".com")
+    ) {
+      alert("Please enter a valid email address.");
     }
-    // users.forEach((user) => {
 
-    // })
-});
+    if (password.value === "" || password.value.length < 8) {
+      alert(
+        "Please enter a valid password. Password must be at least 8 characters long.",
+      );
+    }
+
+    if (email.value === user.email && password.value === user.password) {
+      alert("Login successful! Welcome, " + user.name + "!");
+      // Redirect to the dashboard or home page
+      window.location.href = "Dashboard.html";
+    } else {
+      alert("Invalid email or password. Please try again.");
+    }
+  });
+}
